aa6d55f9f132e5401ead1430d3733bfde08956e5
Vincent Hudry <vincent.hudry@live.fr>
2020-01-22 00:51:24 +0100

    step

--- app/src/main/java/com/vincent/hudry/generictracker/model/form/formElements/NumberElement.java
+++ app/src/main/java/com/vincent/hudry/generictracker/model/form/formElements/NumberElement.java
@@ -1,12 +1,20 @@
 package com.vincent.hudry.generictracker.model.form.formElements;
 
 import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.vincent.hudry.generictracker.R;
 
 import org.json.JSONObject;
 
 public class NumberElement extends FormElement {
+    private View view;
     public NumberElement(Activity activity) {
         super(activity);
+        LayoutInflater layoutInflater = activity.getLayoutInflater();
+        view = layoutInflater.inflate(R.layout.fed_number, null);
+        this.layout = view;
     }
 
     @Override
